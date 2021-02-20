d88a0e1e9c44c811bd9a4205a2c9db6bcc1f1ab5
Leonardo Cunha <leonardo.msc42@gmail.com>
2021-02-20 15:13:27 -0300

    feat: ensure the user is able see the title of all projects created

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/projects', function () {
+    $projects = App\Models\Project::all();
+
+    return view('projects.index', compact('projects'));
+});
+
 Route::post('/projects', function () {
 
     App\Models\Project::create(request(['title', 'description']));
